bf82772e04a8eda61c0e0395fd142e9eb5b3a86d
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-06-15 19:58:51 +0530

    feat:Added FirstUser Invitee Seeder

---
+++ src/seeders/20230615142433-FirstUser_inviteeSeeder.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Invitees', [{
+      inviteeId: 'first_user_inviteeId',
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      phone: '1234567890',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Invitee', null, {});
+  }
+};
